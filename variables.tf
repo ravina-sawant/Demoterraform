7e52eb30388dfb17a60c9d36edac5e3cc5a9f132
ravina-sawant <ravinasawant199@gmail.com>
2025-01-28 17:01:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,5 +2,5 @@
 variable "bucket_name" {
   description = "Enter S3 bucket Name: "
   type        = string
-  default     = "ravina280125"  # You can also set this dynamically
+  default     = "ravina28015525"  # You can also set this dynamically
 }
